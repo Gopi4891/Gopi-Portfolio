247b298dce5d15c5555c4ae4831cc21ee6cf8803
GOPINATH <gopipaaru2004@gmail.com>
2026-01-24 13:28:29 +0530

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-# Stop your application
-# For Node.js with PM2:
-# pm2 stop all
-
-# For killing process by port:
-# sudo kill $(sudo lsof -t -i:3000)
-
-# Generic kill
-pkill -f "node" || true
-# pkill -f "python3" || true
+isExistApp = `pgrep httpd`
+if [[ -n  $isExistApp ]]; then
+    service httpd stop        
+fi
